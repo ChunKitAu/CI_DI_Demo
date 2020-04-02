8113377bb8fd206c602cce09049e59dc808d2d51
ChunKitAu <570137511@qq.com>
2020-04-02 15:55:40 +0800

    Initial commit

---
+++ src/main/java/com/ci_cddemo/demo/test.java
@@ -0,0 +1,19 @@
+package com.ci_cddemo.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @auther ChunKitAu
+ * @create 2020-04-02 02
+ */
+@RestController
+public class test {
+
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
